aab41b59ad40c1e81ddbce62e30caa45563aa494
David Aguado Dianez <daguadod@alpha25.aulas.gsyc.urjc.es>
2017-10-13 15:37:46 +0200

    añadido convertir fichero a json

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler  import SmallSMILHandler
@@ -23,8 +24,13 @@ if __name__ == "__main__":
             for attrs in valores:
                 if valores[attrs] != '': 
                     atributos += '\t' + attrs + '="' + valores[attrs] + '"'
-            exit += names  + atributos + '\n'
+            exit += names + atributos + '\n'
         print(exit)
 
+        file = file.replace('.smil', '.json')
+        with open(file, 'w') as outfile_json:
+            json.dump(sHandler.get_tags(), outfile_json, indent=3, separators=(' ',
+            ': '))
+            
     except IndexError: 
             sys.exit('Usage python3 karaoke.p file.smil')
